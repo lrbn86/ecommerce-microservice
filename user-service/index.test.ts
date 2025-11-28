677daeea9841bd9440e783ac479299335ecf3f52
lrbn86 <brandonnguyendev@gmail.com>
2025-11-27 21:33:48 -0500

    add test stubs

--- user-service/index.test.ts
+++ user-service/index.test.ts
@@ -1,8 +1,53 @@
+import request from 'supertest';
 import { describe, it, expect } from 'vitest';
 
-describe('Test', () => {
-  it('should do a test', () => {
-    expect(5).toBe(5);
+describe('User Service Test', () => {
+  it('should register a user with email and password', async () => {
+    throw '';
+  });
+
+  it('should not register a user if both email and password are not provided', async () => {
+    throw '';
+  });
+
+  it('should not register a user with invalid email format', async () => {
+    throw '';
+  });
+
+  it('should return 409 if email already exists when registering a user', async () => {
+    throw '';
+  });
+
+  it('should login a user with valid email and password', async () => {
+    throw '';
+  });
+
+  it('should not login a user if both email and password are not provided', async () => {
+    throw '';
+  });
+
+  it('should not login a user with invalid email format', async () => {
+    throw '';
+  });
+
+  it('should not login a user with invalid credentials', async () => {
+    throw '';
+  });
+
+  it('should get the right user profile', async () => {
+    throw '';
+  });
+
+  it('should return 401 if token is missing when retrieving user profile', async () => {
+    throw '';
+  });
+
+  it('should return 401 if token is invalid or has expired when retrieving user profile', async () => {
+    throw '';
+  });
+
+  it('should return 403 if the user does not have the right role when retrieving user profile', async () => {
+    throw '';
   });
 });
 
